d1cdfeb726efd39ffa3040ba42243ca75f2a0bf5
nrsabit <mailbox.sabit@gmail.com>
2023-12-03 01:43:51 +0600

    server page updated (in laptop)

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ async function main() {
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`Future Starts is running on Port: ${config.port}`);
     });
   } catch (err) {
     console.log(err);
