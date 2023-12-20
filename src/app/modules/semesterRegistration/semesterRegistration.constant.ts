0e33bd3919aaee252bd9eed9a5f206170c139dc9
nrsabit <mailbox.sabit@gmail.com>
2023-12-21 01:53:07 +0600

    semester registration updating completed

--- src/app/modules/semesterRegistration/semesterRegistration.constant.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.constant.ts
@@ -1,1 +1,6 @@
 export const semesterRegistrationStatus = ['UPCOMING', 'ONGOING', 'ENDED'];
+export const RegistrationStatusObj = {
+  upcoming: 'UPCOMING',
+  ongoing: 'ONGOING',
+  ended: 'ENDED',
+};
--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -4,6 +4,7 @@ import { AcademicSemesterModel } from '../academicSemester/academicSemester.mode
 import { TSemesterRegistration } from './semesterRegistration.interface';
 import { SemesterRegistrationModel } from './semesterRegistration.model';
 import QueryBuilder from '../../builder/QueryBuilder';
+import { RegistrationStatusObj } from './semesterRegistration.constant';
 
 const getAllSemesterRegistrationsService = async (
   query: Record<string, unknown>,
@@ -28,6 +29,19 @@ const getSingleSemesterRegistrationService = async (id: string) => {
 const createSemesterRegistrationService = async (
   payload: TSemesterRegistration,
 ) => {
+  // check if there is alreay any 'UPCOMING' or 'ONGOING' semester registration or not.
+  const isUpcomingOrOngoingSemesterExists =
+    await SemesterRegistrationModel.findOne({
+      $or: [{ status: 'UPCOMING' }, { status: 'ONGOING' }],
+    });
+  if (isUpcomingOrOngoingSemesterExists) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `There is already an ${isUpcomingOrOngoingSemesterExists.status} Semester exists`,
+    );
+  }
+
+  // check if the academicSemester exists or not.
   const academicSemester = payload?.academicSemester;
   const isAcademicSemesterExists =
     await AcademicSemesterModel.findById(academicSemester);
@@ -54,6 +68,48 @@ const updateSemesterRegistrationService = async (
   id: string,
   payload: Partial<TSemesterRegistration>,
 ) => {
+  // check if the semester is exists or not.
+  const currentSemesterRegestration =
+    await SemesterRegistrationModel.findById(id);
+  if (!currentSemesterRegestration) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'Semester Registration Not Found.',
+    );
+  }
+
+  // check if the semester is already ended. (we will not allow to edit that.)
+  const semesterRegistrationStatus = currentSemesterRegestration?.status;
+  const requestedStatus = payload?.status;
+  if (semesterRegistrationStatus === RegistrationStatusObj.ended) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      'Semester Registration is Ended.',
+    );
+  }
+
+  // logic for not allowing status change from 'UPCOMING' to 'ENDED' directly.
+  if (
+    semesterRegistrationStatus === RegistrationStatusObj.upcoming &&
+    requestedStatus === RegistrationStatusObj.ended
+  ) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `You cannot change the status directly from ${semesterRegistrationStatus} to ${requestedStatus}`,
+    );
+  }
+
+  // logic for not allowing status change from 'ONGOING' to 'UPCOMING' directly.
+  if (
+    semesterRegistrationStatus === RegistrationStatusObj.ongoing &&
+    requestedStatus === RegistrationStatusObj.upcoming
+  ) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `You cannot change the status directly from ${semesterRegistrationStatus} to ${requestedStatus}`,
+    );
+  }
+
   const result = await SemesterRegistrationModel.findByIdAndUpdate(
     id,
     payload,
